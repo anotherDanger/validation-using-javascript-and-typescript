f3081712454bb1cd4cffdf4edaa0712d14f46d78
Andhika Danger <andhikadanger666@gmail.com>
2025-01-23 09:34:27 +0700

    username validation flash message

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,50 @@
 const btn = document.getElementById('btn').addEventListener('click', () => {
     const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const flash = document.getElementById('usernameFlash');
+    const passFlash = document.getElementById('passwordFlasher');
     if (!username) {
-        alert('Tidak boleh kosong');
+        flash.innerText = 'Cannot Empty!';
+        return;
+    }
+    //forbidden
+    const invalidChars = /[!@#$%^&*(),.?":{}|<>]/;
+    if (invalidChars.test(username.trim())) {
+        flash.innerText = 'Cannot contains invalid characters!';
         return;
     }
     //length
     const usernameLength = username.trim().length;
     if (usernameLength < 3) {
-        alert('Too Short');
-    }
-    //forbidden
-    const invalidChars = /[!@#$%^&*(),.?":{}|<>]/;
-    if (invalidChars.test(username.trim())) {
-        alert('Contains invalid chars');
+        flash.innerText = 'Too Short!';
         return;
     }
+    //password length
+    if (password.length < 6) {
+        passFlash.innerText = 'Too short';
+        return;
+    }
+    ;
+    //at least one number
+    if (!/\d/.test(password)) {
+        passFlash.innerText = 'Password must contain at least one number!';
+        return;
+    }
+    //at least uppercase letter
+    if (!/[A-Z]/.test(password)) {
+        passFlash.innerText = 'Password must contain at least one uppercase letter!';
+        return;
+    }
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        passFlash.innerText = 'Password must contain at least one special character!';
+        return;
+    }
+    const placeholder = document.getElementById('usernamePlaceholder');
+    placeholder.remove();
+    flash.innerText = 'Valid Username!';
+    flash.classList.remove('text-danger');
+    flash.classList.add('text-light');
+    passFlash.innerText = 'Valid Password!';
+    passFlash.classList.remove('text-danger');
+    passFlash.classList.add('text-light');
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 const btn = (document.getElementById('btn') as HTMLButtonElement).addEventListener('click', () => {
     const username: any = (document.getElementById('username') as HTMLInputElement | null).value;
+    const password: any = (document.getElementById('password') as HTMLInputElement | null).value;
+    const flash = document.getElementById('usernameFlash') as HTMLDivElement;
+    const passFlash = document.getElementById('passwordFlasher') as HTMLDivElement;
     if(!username)
     {
-        alert('Tidak boleh kosong');
+        flash.innerText = 'Cannot Empty!';
+        return;
+    }
+    
+    //forbidden
+    const invalidChars = /[!@#$%^&*(),.?":{}|<>]/;
+    if(invalidChars.test(username.trim())){
+        flash.innerText = 'Cannot contains invalid characters!';
         return;
     }
 
@@ -10,13 +20,47 @@ const btn = (document.getElementById('btn') as HTMLButtonElement).addEventListen
     const usernameLength = username.trim().length;
     if(usernameLength < 3)
     {
-        alert('Too Short');
-    }
-
-    //forbidden
-    const invalidChars = /[!@#$%^&*(),.?":{}|<>]/;
-    if(invalidChars.test(username.trim())){
-        alert('Contains invalid chars');
+        flash.innerText = 'Too Short!';
         return;
     }
+
+    //password length
+    if(password.length < 6)
+    {
+        passFlash.innerText = 'Too short';
+        return;
+    };
+
+    //at least one number
+    if(!/\d/.test(password))
+    {
+        passFlash.innerText = 'Password must contain at least one number!';
+        return;
+    }
+
+    //at least uppercase letter
+    if(!/[A-Z]/.test(password))
+    {
+        passFlash.innerText = 'Password must contain at least one uppercase letter!';
+        return;
+    }
+
+    if(!/[!@#$%^&*(),.?":{}|<>]/.test(password))
+    {
+        passFlash.innerText = 'Password must contain at least one special character!';
+        return;
+    }
+
+    const placeholder = document.getElementById('usernamePlaceholder');
+    placeholder.remove();
+
+    flash.innerText = 'Valid Username!';
+    flash.classList.remove('text-danger');
+    flash.classList.add('text-light');
+
+    passFlash.innerText = 'Valid Password!';
+    passFlash.classList.remove('text-danger');
+    passFlash.classList.add('text-light');
+
+
 })
