96c5f0c7cb17590268ab1de07d248c56eb9378ab
Andhika Danger <andhikadanger666@gmail.com>
2025-01-23 09:35:52 +0700

    main

--- src/main.ts
+++ src/main.ts
@@ -24,43 +24,7 @@ const btn = (document.getElementById('btn') as HTMLButtonElement).addEventListen
         return;
     }
 
-    //password length
-    if(password.length < 6)
-    {
-        passFlash.innerText = 'Too short';
-        return;
-    };
 
-    //at least one number
-    if(!/\d/.test(password))
-    {
-        passFlash.innerText = 'Password must contain at least one number!';
-        return;
-    }
-
-    //at least uppercase letter
-    if(!/[A-Z]/.test(password))
-    {
-        passFlash.innerText = 'Password must contain at least one uppercase letter!';
-        return;
-    }
-
-    if(!/[!@#$%^&*(),.?":{}|<>]/.test(password))
-    {
-        passFlash.innerText = 'Password must contain at least one special character!';
-        return;
-    }
-
-    const placeholder = document.getElementById('usernamePlaceholder');
-    placeholder.remove();
-
-    flash.innerText = 'Valid Username!';
-    flash.classList.remove('text-danger');
-    flash.classList.add('text-light');
-
-    passFlash.innerText = 'Valid Password!';
-    passFlash.classList.remove('text-danger');
-    passFlash.classList.add('text-light');
 
 
 })
